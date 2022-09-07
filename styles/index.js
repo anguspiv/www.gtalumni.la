6942d0be50875f8950728df2cbba31ba505bed3e
Angus <angusp@angusp.com>
2022-09-06 20:18:25 -0700

    chore: adds emotion styling
    Adds emotion and polished for styling
    
    closes #16

---
+++ styles/index.js
@@ -0,0 +1,60 @@
+import { css, Global, keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const globalStyles = (
+  <Global
+    styles={css`
+      html,
+      body {
+        padding: 3rem 1rem;
+        margin: 0;
+        background: papayaWhip;
+        font-family: Helvetica, Arial, sans-serif;
+        font-size: 16px;
+        line-height: 1.5;
+      }
+    `}
+  />
+);
+
+export const basicStyles = css`
+  background-color: white;
+  color: cornflowerblue;
+  border: 1px solid lightgreen;
+  border-right: none;
+  border-bottom: none;
+  box-shadow: 5px 5px 0 0 lightgreen, 10px 10px 0 0 lightyellow;
+  transition: all 0.2s ease-in-out;
+  margin: 3rem 0;
+  padding: 1rem 0.5rem;
+`;
+
+export const hoverStyles = css`
+  &:hover {
+    color: white;
+    background-color: lightgray;
+    border-color: aqua;
+    box-shadow: -15px -15px 0 0 aqua, -30px -30px 0 0 cornflowerblue;
+  }
+`
+
+export const Basic = styled.div`
+  ${basicStyles};
+`;
+
+export const Combined = styled.div`
+  ${basicStyles};
+  ${hoverStyles};
+  & code {
+    background-color: linen;
+  }
+`;
+
+export const Animated = styled.div`
+  ${basicStyles};
+  ${hoverStyles};
+  & code {
+    background-color: linen;
+  }
+  animation: ${({ animation }) => animation} 0.2s infinite ease-in-out alternate;
+`;
