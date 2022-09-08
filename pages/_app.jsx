d63d4b92018743fdfac85662c509a02690c3bfff
Angus <angusp@angusp.com>
2022-09-07 22:34:06 -0700

    feat: adds google analytics
    Adds Google Analytics for user tracking
    
    closes #3

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
+import Script from 'next/script';
 import { globalStyles } from '../styles';
 
 const cache = createCache({ key: 'css', prepend: true });
@@ -8,6 +9,21 @@ const cache = createCache({ key: 'css', prepend: true });
 function App({ Component, pageProps }) {
   return (
     <CacheProvider value={cache}>
+      <Script
+        id="gtag"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload" id="gtag-init">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `}
+      </Script>
       {globalStyles}
       <Component {...pageProps} />
     </CacheProvider>
