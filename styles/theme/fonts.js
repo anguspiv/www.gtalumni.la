3809bda143a1f4993a961118b09e3f9002b01d18
Angus <angusp@angusp.com>
2022-09-11 14:47:20 -0700

    feat(theme): adds google fonts
    Adds Roboto, Roboto Condensed, and Roboto Slab fonts to the base styles
    and site theme
    
    closes #50

---
+++ styles/theme/fonts.js
@@ -0,0 +1,8 @@
+import { css } from '@emotion/react';
+
+export const fonts = css`
+  --gt-font-family-regular: 'Roboto', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
+  --gt-font-family-title: 'Roboto Slab', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
+`;
+
+export default fonts;
