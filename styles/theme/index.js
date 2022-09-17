25d7a1fec4625bfa3f7abc753e014a45fe00042b
Angus <angusp@angusp.com>
2022-09-17 10:53:42 -0700

    feat(theme): adds base transition vars
    Adds base transition css vars

--- styles/theme/index.js
+++ styles/theme/index.js
@@ -2,9 +2,16 @@ import { css } from '@emotion/react';
 import { colors } from './colors';
 import { fonts } from './fonts';
 
+export const animation = css`
+  --gt-transition-duration-base: 0.2s;
+  --gt-transition-timing-base: ease-in-out;
+  --gt-transition-base: all 0.2s ease-in-out;
+`;
+
 export const theme = css`
   ${colors}
   ${fonts}
+  ${animation}
 `;
 
 export default theme;
