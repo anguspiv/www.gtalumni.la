57f178011429d17c2d15a0874981056d95bff62d
Angus <angusp@angusp.com>
2022-12-02 17:00:46 -0800

    refactor: moves pages to src folder
    Moves the pages directory to `src/pages` to keep all scripts within src

--- __tests__/pages/index.test.jsx
+++ __tests__/pages/index.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import IndexPage from '../../pages/index';
+import IndexPage from '../../src/pages/index';
 
 describe('<IndexPage />', () => {
   it('renders without crashing', () => {
