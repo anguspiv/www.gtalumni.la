03023a13c29342df9c06040b14d23a6dcc2b1354
Angus <angusp@angusp.com>
2022-12-02 17:33:13 -0800

    refactor: fixes storybook documentation
    Fixes the storybook documentation to work with Next.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,33 @@
-import { RouterContext } from 'next/dist/shared/lib/router-context';
-import { Global } from '@emotion/react';
-import { globalStyles } from '../styles/';
+import { Global, ThemeProvider } from '@emotion/react';
+import * as NextImage from 'next/image';
+import { globalStyles } from '@styles/global';
+import { theme } from '@styles/theme';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage
+      {...props}
+      unoptimized
+      blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAADAAQDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAbEAADAAMBAQAAAAAAAAAAAAABAgMABAURUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAFxEAAwEAAAAAAAAAAAAAAAAAAAECEf/aAAwDAQACEQMRAD8Anz9voy1dCI2mectSE5ioFCqia+KCwJ8HzGMZPqJb1oPEf//Z"
+    />
+  ),
+});
+
+export const decorators = [
+  (Story) => {
+    return (
+      <ThemeProvider theme={theme}>
+        <Global styles={globalStyles} />
+        <Story />
+      </ThemeProvider>
+    );
+  },
+];
 
 export const parameters = {
-  nextRouter: {
-    Provider: RouterContext.Provider,
-  },
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
@@ -14,14 +36,3 @@ export const parameters = {
     },
   },
 };
-
-export const decorators = [
-  (Story) => {
-    return (
-      <>
-        <Global styles={globalStyles} />
-        <Story />
-      </>
-    );
-  },
-];
