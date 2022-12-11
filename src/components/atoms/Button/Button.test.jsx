2629a88c4624647eabb1a819816011897b3196c5
Angus <angusp@angusp.com>
2022-12-10 21:59:02 -0800

    feat(button): add style variant and as prop
    Adds the Secondary Styled button variant. Also adds the `as` prop to apply
    button styles to other props.

--- src/components/atoms/Button/Button.test.jsx
+++ src/components/atoms/Button/Button.test.jsx
@@ -2,7 +2,7 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { Button } from './Button';
 
 describe('<Button />', () => {
-  it('should render', () => {
+  it('should render a button', () => {
     expect.assertions(2);
 
     const onClick = jest.fn();
@@ -14,5 +14,37 @@ describe('<Button />', () => {
     fireEvent.click(screen.getByRole('button', { name: 'My Button' }));
 
     expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render different components as buttons', () => {
+    expect.assertions(3);
+
+    const onClick = jest.fn();
+
+    render(
+      <Button as="a" onClick={onClick} href="https://example.com">
+        My Button
+      </Button>,
+    );
+
+    expect(screen.getByRole('link', { name: 'My Button' })).toBeInTheDocument();
+
+    fireEvent.click(screen.getByRole('link', { name: 'My Button' }));
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+
+    // eslint-disable-next-line react/prop-types
+    function Foo({ children, ...props }) {
+      return <h1 {...props}>{children}</h1>;
+    }
+
+    render(
+      // eslint-disable-next-line react/jsx-no-bind
+      <Button as={Foo} onClick={onClick}>
+        Foo
+      </Button>,
+    );
+
+    expect(screen.getByRole('heading')).toHaveTextContent('Foo');
   });
 });
