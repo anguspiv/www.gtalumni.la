e820a4920a6529a76ce47db01b3d8658ddb6c670
Angus <angusp@angusp.com>
2022-12-20 21:00:30 -0800

    test: adds e2e test for global elements
    re #49

---
+++ cypress/e2e/global.cy.js
@@ -0,0 +1,39 @@
+describe('Global Elements', () => {
+  beforeEach(() => {
+    cy.visit('/404', { failOnStatusCode: false });
+  });
+
+  it('should have the site banner', () => {
+    cy.findByRole('banner')
+      .findByAltText('Georgia Tech Alumni Association of Los Angeles')
+      .parent()
+      .click();
+
+    cy.location('pathname').should('eq', '/');
+  });
+
+  it('it should display the site navigation', () => {
+    cy.findByRole('navigation').should('exist');
+
+    cy.findByRole('link', { name: 'Events' }).click();
+
+    cy.location('pathname').should('eq', '/events');
+  });
+
+  it('should display the footer', () => {
+    cy.findByRole('contentinfo').should('exist');
+
+    cy.findByText('Contact Us').should('exist');
+    cy.findByRole('link', { name: 'Facebook' }).should('exist');
+    cy.findByRole('link', { name: 'Email' }).should('exist');
+    cy.findByRole('link', { name: 'GitHub' }).should('exist');
+
+    const year = new Date().getFullYear();
+
+    cy.findByText(`© ${year} Georgia Tech Alumni Association of Los Angeles`).should('exist');
+
+    cy.findByText('Neighbor Alumni Associations').should('exist');
+    cy.findByRole('link', { name: 'Orange County' }).should('exist');
+    cy.findByRole('link', { name: 'San Diego' }).should('exist');
+  });
+});
