446f288af00f5e34fcd78757c21afb68f1222862
Angus <angusp@angusp.com>
2023-09-22 16:36:27 -0700

    feat(contact-section): adds the discord server invite link
    Adds the Discord Server link invite to the contact us section
    
    re #225

--- src/components/organisms/ContactSection/ContactSection.test.jsx
+++ src/components/organisms/ContactSection/ContactSection.test.jsx
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import { DISCORD_INVITE_LINK, FACEBOOK_GROUP_LINK } from '@constants/social';
 import { ContactSection } from './ContactSection';
 
 describe('<ContactSection />', () => {
   it('should render the contact section', () => {
-    expect.assertions(4);
+    expect.assertions(5);
     render(<ContactSection />);
     expect(screen.getByRole('heading')).toHaveTextContent('Contact Us');
     expect(
@@ -13,7 +14,11 @@ describe('<ContactSection />', () => {
     ).toBeInTheDocument();
     expect(screen.getByRole('link', { name: 'Facebook Group' })).toHaveAttribute(
       'href',
-      'https://www.facebook.com/groups/39515948772/',
+      FACEBOOK_GROUP_LINK,
+    );
+    expect(screen.getByRole('link', { name: 'Discord Server' })).toHaveAttribute(
+      'href',
+      DISCORD_INVITE_LINK,
     );
     expect(screen.getByRole('link', { name: 'hello@gtalumni.la' })).toHaveAttribute(
       'href',
